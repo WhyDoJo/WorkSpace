6d7b476ef6397015183df3c89b3c14307e41bb4c
WhyDojo <grinvaurdourin@gmail.com>
2026-05-08 04:16:26 +0300

    работа с api получали данные для select

---
+++ src/js/components/select.js
@@ -0,0 +1,34 @@
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector("#city");
+  const cityChoices = new Choices(citySelect, {
+    searchEnabled: false,
+    itemSelectText: "",
+    shouldSort: false,
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map((location) => ({ value: location }));
+      cityChoices.setChoices(locations, "value", "label", true);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+
+init();
--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,3 @@ import "./_libs.js";
 // Подключение компонентов
 import "./_components.js";
 
-const citySelect = document.querySelector("#city");
-
-if (citySelect && window.Choices) {
-  new window.Choices(citySelect, {
-    searchEnabled: false,
-    itemSelectText: "",
-    shouldSort: false,
-  });
-}
